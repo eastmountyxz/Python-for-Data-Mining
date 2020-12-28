27ab64375d15b55d38f7bcaeeb9ec7b24bf15401
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-12-28 11:35:25 +0800

    Update test01-svm.py

--- blog27-SVM&WineDataset/test01-svm.py
+++ blog27-SVM&WineDataset/test01-svm.py
@@ -5,7 +5,7 @@ X = np.array([[-1, -1], [-2, -2], [1, 3], [4, 6]])
 y = np.array([1, 1, 2, 2])
 clf = SVC()  
 clf.fit(X, y)   
-print clf.fit(X,y)  
+print(clf.fit(X,y))
 print(clf.predict([[-0.8,-1], [2,1]]))
 
 #输出结果：[1, 2]
