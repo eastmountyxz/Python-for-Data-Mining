354ff9791713abf89d168302dc12e7be53361d3d
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-12-28 11:42:09 +0800

    Update test02-wordcloud.py

--- blog26-SnowNLP/test02-wordcloud.py
+++ blog26-SnowNLP/test02-wordcloud.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
  
 #打开本体TXT文件
-text = open('data.txt').read()
-print type(text)
+text = open('data.txt',encoding='utf-8').read()
+print(type(text))
  
 #结巴分词 cut_all=True 设置为精准模式 
 wordlist = jieba.cut(text, cut_all = False)
  
 #使用空格连接 进行中文分词
 wl_space_split = " ".join(wordlist)
-print wl_space_split
+print(wl_space_split)
  
 #对分词后的文本生成词云
 my_wordcloud = WordCloud().generate(wl_space_split)
