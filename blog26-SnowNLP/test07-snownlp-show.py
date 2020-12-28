4ce91ba0484e3fb90ff8bef8db674dbbdacb1a1f
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-12-28 11:48:59 +0800

    Update test07-snownlp-show.py

--- blog26-SnowNLP/test07-snownlp-show.py
+++ blog26-SnowNLP/test07-snownlp-show.py
@@ -3,11 +3,11 @@ from snownlp import SnowNLP
 import codecs
 import os
 
-source = open("data.txt","r")
+source = open("data.txt","r", encoding='utf-8')
 line = source.readlines()
 sentimentslist = []
 for i in line:
-    s = SnowNLP(i.decode("utf-8"))
+    s = SnowNLP(i)
     print(s.sentiments)
     sentimentslist.append(s.sentiments)
 
