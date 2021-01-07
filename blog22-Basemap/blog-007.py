6e61e44352cb3bf14a96787554d748f5fbbff626
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-06 21:33:52 -0600

    Add files via upload

---
+++ blog22-Basemap/blog-007.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80,
+            llcrnrlon=-180, urcrnrlon=180)
+m.drawcoastlines()
+plt.show()
