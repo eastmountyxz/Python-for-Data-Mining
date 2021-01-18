71978ae643a25eab10f63ca999dc14b77245c073
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-18 22:58:09 +0800

    Add files via upload

---
+++ blog19-Iris/test04-kde.py
@@ -0,0 +1,13 @@
+import pandas
+import matplotlib.pyplot as plt
+
+#导入数据集iris
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+#读取csv数据
+dataset = pandas.read_csv(url, names=names)
+print(dataset.describe())
+
+dataset.plot(kind='kde')
+plt.show()
