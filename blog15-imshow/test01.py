138b2ddbc737114ec8980415588d4c947a3571fc
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-02-27 13:21:16 +0800

    Create test01.py

---
+++ blog15-imshow/test01.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from matplotlib import pyplot as plt
+ 
+fig = plt.figure()
+ax1 = fig.add_subplot(231)
+ax2 = fig.add_subplot(232)
+ax3 = fig.add_subplot(233)
+ax4 = fig.add_subplot(234) 
+ax5 = fig.add_subplot(235)
+ax6 = fig.add_subplot(236)
+plt.grid(True)
+plt.show()
