bae3d3a63dc51b093d03372a81c5c637617c5158
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-03-06 20:56:57 +0800

    Create test01.py

---
+++ blog10-Pandas/test01.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 06 10:55:17 2017
+@author: eastmount
+"""
+ 
+import pandas as pd
+data = pd.read_csv("data.csv",index_col='year') #index_col用作行索引的列名 
+#显示前6行数据 
+print(data.shape)  
+print(data.head(6))
+ 
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['simHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False   #用来正常显示负号
+data.plot()
+plt.savefig(u'时序图.png', dpi=500)
+plt.show()
